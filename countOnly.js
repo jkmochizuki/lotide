ed7012f498d228f9f3af7044090c412a5b1ab444
Juliana Mochizuki <jkmochizuki@gmail.com>
2023-02-08 14:22:06 -0800

    notes removed

--- countOnly.js
+++ countOnly.js
@@ -2,13 +2,6 @@ const assertEqual = function(actual, expected) {
   console.log(actual === expected ? `✅✅✅ Assertion Passed: ${actual} === ${expected}` : `🔴🔴🔴 Assertion Failed: ${actual} !== ${expected}`);
 };
 
-// Input: array and object
-// Output: Object {
-//  inputObj.key : Number => # of inputObj.key is in the inputArray,
-// Keys with falsy values are not counted
-// Keys not included in the array are not counted
-// Keys are truthy => count how many of the key are in the array
-
 const countOnly = function(allItems, itemsToCount) {
   const results = {};
   for (const item of allItems) {
