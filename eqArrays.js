989ec71caa91767558c898e8137e2579fd895a50
Juliana Mochizuki <jkmochizuki@gmail.com>
2023-02-10 14:22:18 -0800

    code adjusted and refactored

--- eqArrays.js
+++ eqArrays.js
@@ -3,11 +3,15 @@ const assertEqual = function(actual, expected) {
 };
 
 const eqArrays = function(arr1, arr2) {
-  return arr1.toString() === arr2.toString() ? true : false;
+  if (arr1.length !== arr2.length) return false;
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) return false;
+  }
+  return true;
 };
 
 // TEST CODE
 assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
-assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), true);
-assertEqual(eqArrays([1, 2, 3], [3, 2]), true);
+assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false);
+assertEqual(eqArrays([1, 2, 3], [3, 2]), false);
 assertEqual(eqArrays([], [3, 2]), false);
