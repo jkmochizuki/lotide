9b7c7aa8759bf06b02e1c7cfee5713fa608c08f4
Juliana Mochizuki <jkmochizuki@gmail.com>
2023-02-10 15:07:32 -0800

    eqArrays function adjusted

--- assertObjectsEqual.js
+++ assertObjectsEqual.js
@@ -1,5 +1,9 @@
 const eqArrays = function(arr1, arr2) {
-  return arr1.toString() === arr2.toString() ? true : false;
+  if (arr1.length !== arr2.length) return false;
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) return false;
+  }
+  return true;
 };
 
 const eqObjects = function(object1, object2) {
