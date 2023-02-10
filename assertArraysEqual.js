2d15cb08bac35bfc40091792aa74082b7e8e03fd
Juliana Mochizuki <jkmochizuki@gmail.com>
2023-02-10 15:02:58 -0800

    eqArrays function adjusted

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,10 +1,12 @@
 const eqArrays = function(arr1, arr2) {
-  return arr1.toString() === arr2.toString() ? true : false;
+  if (arr1.length !== arr2.length) return false;
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) return false;
+  }
+  return true;
 };
 
 const assertArraysEqual = function(arr1, arr2) {
   let result = eqArrays(arr1, arr2);
   console.log(result === true ? `✅✅✅ Assertion Passed: ${arr1} === ${arr2}` : `🔴🔴🔴 Assertion Failed: ${arr1} !== ${arr2}`);
 };
-
-assertArraysEqual;
