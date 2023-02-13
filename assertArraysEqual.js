d090887405f0cbd5d3e7923356d8e745c0100d45
Juliana Mochizuki <jkmochizuki@gmail.com>
2023-02-13 13:24:34 -0800

    refactoed assertArraysEqual

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,12 +1,10 @@
-const eqArrays = function(arr1, arr2) {
-  if (arr1.length !== arr2.length) return false;
-  for (let i = 0; i < arr1.length; i++) {
-    if (arr1[i] !== arr2[i]) return false;
-  }
-  return true;
-};
+const eqArrays = require('./eqArrays');
 
 const assertArraysEqual = function(arr1, arr2) {
   let result = eqArrays(arr1, arr2);
-  console.log(result === true ? `✅✅✅ Assertion Passed: ${arr1} === ${arr2}` : `🔴🔴🔴 Assertion Failed: ${arr1} !== ${arr2}`);
+  console.log(result === true ?
+    `✅✅✅ Assertion Passed: ${arr1} === ${arr2}` :
+    `🔴🔴🔴 Assertion Failed: ${arr1} !== ${arr2}`);
 };
+
+module.exports = assertArraysEqual;
