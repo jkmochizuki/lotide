bf3433ebfe9ffaf79284e0f4ef6a51c8238232e4
Juliana Mochizuki <jkmochizuki@gmail.com>
2023-02-13 08:34:49 -0800

    refactored tail

--- tail.js
+++ tail.js
@@ -1,15 +1,5 @@
-const assertEqual = function(actual, expected) {
-  return actual === expected ? `✅✅✅ Assertion Passed: ${actual} === ${expected}` : `🔴🔴🔴 Assertion Failed: ${actual} !== ${expected}`;
-};
-
 const tail = function(arr) {
-  let newArray;
-  newArray = arr.slice(1, arr.length);
-  return newArray;
+  return arr.slice(1);
 };
 
-// TEST CODE
-
-const result = tail(["Hello", "Lighthouse", "Labs"]);
-console.log(assertEqual(result[0], "Lighthouse"));
-console.log(assertEqual(result[1], "Labs"));
+module.exports = tail;
---
+++ test/tailTest.js
@@ -0,0 +1,5 @@
+const tail = require('../tail');
+
+// TEST CODE
+console.log(tail(["Hello", "Lighthouse", "Labs"]));
+console.log(tail([2, 5, 4, 6]));
