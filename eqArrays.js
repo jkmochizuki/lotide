d7a36daf3de2aba3d4298d215b532f9b667d4d5b
Juliana Mochizuki <jkmochizuki@gmail.com>
2023-02-13 13:11:04 -0800

    adjusted eqArrays function

--- eqArrays.js
+++ eqArrays.js
@@ -1,19 +1,16 @@
-const assertEqual = require('./assertEqual');
-
 const eqArrays = function(arr1, arr2) {
   if (arr1.length !== arr2.length) return false;
+
   for (let i = 0; i < arr1.length; i++) {
     const item1 = arr1[i];
     const item2 = arr2[i];
     const notArray = !Array.isArray(item1);
     if (Array.isArray(item1) && !Array.isArray(item2)) return false;
-    if (Array.isArray(item1)) eqArrays(item1, item2);
+    if (Array.isArray(item1) && !eqArrays(item1, item2)) return false;
+    if (Array.isArray(item1) || Array.isArray(item2)) eqArrays(item1, item2);
     if (notArray) if (item1 !== item2) return false;
-    if (!eqArrays(item1, item2)) return false;
   }
   return true;
 };
 
 module.exports = eqArrays;
-
-
